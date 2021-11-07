689842adc08ef2775b33a308f6d0f8438325e99c
AlexandreDFM <91661544+AlexandreDFM@users.noreply.github.com>
2021-11-07 15:42:00 +0100

    Update ProjectionVideo.py

--- ProjectionVideo.py
+++ ProjectionVideo.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python3
+##
+## EPITECH PROJECT, 2021
+## MuseomixProject
+## File description:
+## Project a video on a projector with Raspberry Pi
+##
+
 import subprocess
 from apscheduler.schedulers.blocking import BlockingScheduler
 import datetime
@@ -7,7 +15,7 @@ now = datetime.datetime.now()
 started = 0
 def start_vid(started):
     if (started == 0): 
-        p = subprocess.Popen(["vlc",'--loop', '--fullscreen','NameOfTheFile.mp4'])              
+        p = subprocess.Popen(["vlc",'--loop', '--fullscreen','NameOfTheFile.mp4'])
         started = 1
 
 @sched.scheduled_job('cron', minute=now.minute+1)
